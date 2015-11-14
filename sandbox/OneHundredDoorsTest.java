e5ce2438a913ab41816987584634dbb39f9ea8c9
deer_7A8CD7C2AB <deer@cyber-dojo.org>
2015-11-14 18:41:16 +0000

    12

--- sandbox/OneHundredDoorsTest.java
+++ sandbox/OneHundredDoorsTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.*;
 public class OneHundredDoorsTest {
 
     @Test
-    public void after_the_last_pass_the_state_of_the_door_should_be_Open_if_there_is_only_one_door() {
+    public void after_the_last_pass_the_state_of_the_door_should_be_Open_if_only_passing_one_door() {
         // Given
         OneHundredDoors oneHundredDoors = new OneHundredDoors();
 
@@ -13,6 +13,15 @@ public class OneHundredDoorsTest {
         assertEquals("O", oneHundredDoors.statesOfDoorsAfterPassingPartialDoors(1));
     }
 
-    // TODO: after_the_last_pass_the_state_of_the_doors_should_be_OpenClosed_respectively_if_there_are_two_doors
+    @Test
+    public void after_the_last_pass_the_state_of_the_doors_should_be_OpenClosed_respectively_if_only_passing_two_doors() {
+        // Given
+        OneHundredDoors oneHundredDoors = new OneHundredDoors();
+
+        // When
+        // Then
+        assertEquals("O", oneHundredDoors.statesOfDoorsAfterPassingPartialDoors(1));
+    }
+
     // TODO: after_the_last_pass_the_state_of_the_doors_should_be_OpenClosedClosed_respectively_if_there_are_three_doors
 }
