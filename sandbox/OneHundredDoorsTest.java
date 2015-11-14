f2054d61fc942ef790e4f48566bca02cf4077235
deer_7A8CD7C2AB <deer@cyber-dojo.org>
2015-11-14 18:02:48 +0000

    5

--- sandbox/OneHundredDoorsTest.java
+++ sandbox/OneHundredDoorsTest.java
@@ -9,4 +9,8 @@ public class OneHundredDoorsTest {
         int actual = OneHundredDoors.answer();
         assertEquals(expected, actual);
     }
+
+    // TODO: after_the_last_pass_the_state_of_the_door_should_be_Open_if_there_is_only_one_door
+    // TODO: after_the_last_pass_the_state_of_the_doors_should_be_OpenClosed_respectively_if_there_are_two_doors
+    // TODO: after_the_last_pass_the_state_of_the_doors_should_be_OpenClosedClosed_respectively_if_there_are_three_doors
 }
