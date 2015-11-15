a0440be70813dcf59496901d8506ba672986e473
deer_7A8CD7C2AB <deer@cyber-dojo.org>
2015-11-15 04:50:05 +0000

    25

--- sandbox/OneHundredDoors.java
+++ sandbox/OneHundredDoors.java
@@ -1,17 +1,19 @@
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class OneHundredDoors {
 
     public String statesOfDoorsAfterPassingPartialDoors(int amountOfDoorsPassed) {
+        List<String> doors = new ArrayList<String>(Arrays.asList(
+            "C", "C", "C"
+            ));
         
         if (amountOfDoorsPassed == 1) {
-            List<String> doors = new ArrayList<String>(amountOfDoorsPassed + 1);
             doors.set(1, "O"); 
             return doors.get(1);
         }
         if (amountOfDoorsPassed == 2) {
-            List<String> doors = new ArrayList<String>(amountOfDoorsPassed + 1);
             doors.set(1, "O"); 
             doors.set(2, "C"); 
             return doors.get(1) + doors.get(2);
