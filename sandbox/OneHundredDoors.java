0793f3e0da199cc9dcd6708574f2b4cac2bb68df
deer_7A8CD7C2AB <deer@cyber-dojo.org>
2015-11-15 03:56:38 +0000

    15

--- sandbox/OneHundredDoors.java
+++ sandbox/OneHundredDoors.java
@@ -2,7 +2,7 @@
 public class OneHundredDoors {
 
     public String statesOfDoorsAfterPassingPartialDoors(int amountOfDoorsPassed) {
-        if (amountOfDoorsPassed == 1) return "O"
+        if (amountOfDoorsPassed == 1) return "O";
         else return "OC";
     }
 }
