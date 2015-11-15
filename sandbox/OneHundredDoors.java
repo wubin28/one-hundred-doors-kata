4fe262b37d75167642f6c19f369039566255afdb
deer_7A8CD7C2AB <deer@cyber-dojo.org>
2015-11-15 04:33:35 +0000

    20

--- sandbox/OneHundredDoors.java
+++ sandbox/OneHundredDoors.java
@@ -2,6 +2,7 @@
 public class OneHundredDoors {
 
     public String statesOfDoorsAfterPassingPartialDoors(int amountOfDoorsPassed) {
+        List<String> doors = new ArrayList<String>(amountOfDoorsPassed);
         if (amountOfDoorsPassed == 1) return "O";
         if (amountOfDoorsPassed == 2) return "OC";
         return "OCC";
